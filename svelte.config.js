37632839b71541fdcb05137ca7d9df4377b6d944
jankstar <jankstar.berlin@gmail.com>
2022-10-22 20:07:29 +0200

    redefine build vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,24 @@
 import sveltePreprocess from "svelte-preprocess";
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+//		adapter: adapter({
+//			fallback: 'index.html'
+//		}),
+		alias: {},
+		appDir: '_app',
+		outDir: '.svelte-kit',
+		paths: {
+			assets: '',
+			base: '/project_manager'
+		  }
+	},
 
-export default {
+
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
 };
+
+export default config;
